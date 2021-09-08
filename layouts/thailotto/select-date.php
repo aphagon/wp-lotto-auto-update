210ee3f8f5dfe8b5d913b70a114fab24c32da8c1
Puk-Luk <aphagon@gmail.com>
2021-09-08 13:44:33 +0700

    fix param date

--- layouts/thailotto/select-date.php
+++ layouts/thailotto/select-date.php
@@ -100,8 +100,7 @@ if (!function_exists('wp_lotto_auto_update_container_thailotto_layout_select_dat
 			function clickWpLottoAutoUpdateThaiLottoSelectDate() {
 				const date = document.getElementById("WpLottoAutoUpdateThaiLottoSelectDate").value;
 				if (date != '') {
-					let linkStr = '<?php echo esc_url(add_query_arg(['lotto-date' => '#string'], get_permalink(Page::optionID('thailotto')))); ?>'
-					linkStr.replace("#string#", date);
+					let linkStr = '<?php echo esc_url(add_query_arg(['lotto-date' => '#string#'], get_permalink(Page::optionID('thailotto')))); ?>';
 					window.location = linkStr.replace("#string#", date);
 				} else {
 					alert('<?php _e('จำเป็นต้องเลือกงวด', 'wp-lotto-auto-update'); ?>');
